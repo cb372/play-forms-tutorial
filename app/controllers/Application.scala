d473bee6c2a1de4303f0798159a21c319e40b6a4
Chris Birchall <chris.birchall@guardian.co.uk>
2015-12-23 12:38:56 +0000

    Create a form definition and pass a blank form to the template

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -3,6 +3,8 @@ package controllers
 import models.Widget
 
 import play.api._
+import play.api.data._
+import play.api.data.Forms._
 import play.api.mvc._
 
 import scala.collection.mutable.ArrayBuffer
@@ -20,10 +22,27 @@ class Application extends Controller {
   }
 
   def listWidgets = Action {
-    Ok(views.html.listWidgets(widgets.toSeq))
+    // Pass an unpopulated form to the template
+    Ok(views.html.listWidgets(widgets.toSeq, Application.createWidgetForm))
   }
 
   // This will be the action that handles our form post
   def createWidget = TODO
+
+}
+
+object Application {
+
+  /** The form definition for the "create a widget" form.
+   *  It specifies the form fields and their types,
+   *  as well as how to convert from a Widget to form data and vice versa.
+   */
+  val createWidgetForm = Form(
+    mapping(
+      "name" -> text,
+      "price" -> number
+    )(Widget.apply)(Widget.unapply)
+
+)
 
 }
