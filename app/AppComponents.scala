f73918a2cc02eec39fa0b036ec15c7fdbbadf196
Chris Birchall <chris.birchall@guardian.co.uk>
2015-12-31 10:58:00 +0000

    Add CSRF protection
    * Add a CSRF filter to check our POST request contains a valid token
    * Add a CSRF form helper to add the token as a hidden field
    
    Unfortunately this doesn't actually compile, due to a Play bug related
    to compile-time DI. I'll work around that in the next commit.

--- app/AppComponents.scala
+++ app/AppComponents.scala
@@ -3,10 +3,14 @@ import play.api.ApplicationLoader.Context
 import play.api.BuiltInComponentsFromContext
 import play.api.i18n.{DefaultLangs, DefaultMessagesApi, MessagesApi}
 import play.api.routing.Router
+import play.api.http.HttpFilters
+import play.filters.csrf._
+import play.filters.csrf.CSRF.ConfigTokenProvider
 import router.Routes // this is the auto-generated Router class
 
-class AppComponents(context: Context) extends BuiltInComponentsFromContext(context) {
+class AppComponents(context: Context) extends BuiltInComponentsFromContext(context) with CSRFComponents {
 
+  override lazy val httpFilters = Seq(csrfFilter)
   val messagesApi: MessagesApi = new DefaultMessagesApi(environment, configuration, new DefaultLangs(configuration))
   val appController = new Application(messagesApi)
   val assets = new controllers.Assets(httpErrorHandler)
--- build.sbt
+++ build.sbt
@@ -4,3 +4,4 @@ scalaVersion := "2.11.7"
 lazy val root = (project in file(".")).enablePlugins(PlayScala)
 resolvers += "scalaz-bintray" at "http://dl.bintray.com/scalaz/releases"
 routesGenerator := InjectedRoutesGenerator
+libraryDependencies += filters
